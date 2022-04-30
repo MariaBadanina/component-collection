704695b296c4a33d73295868e6bcb555f45aa218
Maria Badanina <maria.badanina@secuoyas.com>
2022-04-30 15:04:27 +0200

    Storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  stories: [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+};
